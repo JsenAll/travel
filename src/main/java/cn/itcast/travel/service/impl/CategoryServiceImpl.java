3bbbfc1788e330d4a66e4552c74ecd180573de8f
jhs <724033319@qq.com>
2019-08-16 11:12:19 +0800

    动态加载 分类   查数据库 输出到页面 形成html   servlet->service->dao

---
+++ src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.itcast.travel.service.impl;
+
+import cn.itcast.travel.dao.CategoryDao;
+import cn.itcast.travel.dao.impl.CategoryDaoImpl;
+import cn.itcast.travel.domain.Category;
+import cn.itcast.travel.service.CategoryService;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService {
+    private CategoryDao categoryDao = new CategoryDaoImpl();
+
+
+    @Override
+    public List<Category> findAll() {
+        List<Category> all = categoryDao.findAll();
+        return all;
+    }
+}
